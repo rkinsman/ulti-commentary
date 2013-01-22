929fc99ac8197c9d923ba5b0130ab7a44c9e039b
Ross Kinsman <kinsman.ross@gmail.com>
2013-01-22 11:26:39 -0500

    generated new rails app

---
+++ ulti/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ulti::Application.config.secret_token = '9d2e54bc3ce6b4a53f9aa6c2078317ad6c98adcc525450003faf0a62eaca0d7e2b0d96242e197d2136ba9eb0ad12f81146e1fd33cd18f5516486227a092a5a8c'
